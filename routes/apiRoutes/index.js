27ef097a42e9e8c204e025f70ef22af1c4ae9ad8
Robert Pliszka <robplisz@gmail.com>
2022-05-13 11:46:03 -0400

    Added routes for the zookeepers

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeepersRoutes = require('./zookeeperRoutes');
 
 router.use(animalRoutes);
+router.use(zookeepersRoutes);
 
 module.exports = router;
